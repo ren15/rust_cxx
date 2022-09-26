6343d74428059cf0f2ef73712f7a6d939350fd78
iamwrm <iamwrm@gmail.com>
2022-09-26 03:21:34 +0000

    ignore apt stdout

--- ci/deb_install.sh
+++ ci/deb_install.sh
@@ -1,9 +1,9 @@
-apt update
-apt install -y \
+apt-get update 1>/dev/null
+apt-get install -y \
     curl \
     build-essential \
     llvm \
-    clang
+    clang 1>/dev/null
 
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
